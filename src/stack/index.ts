e2500d25f78ea032a087f03acd2c24e7c0efd059
henry <48340699+imwh0im@users.noreply.github.com>
2023-12-28 14:08:32 +0000

    stack

---
+++ src/stack/index.ts
@@ -0,0 +1,42 @@
+export class stack<T>{
+  constructor(
+    private maxSize: number,
+  ) {}
+
+  private items: T[] = [];
+
+  isFull() {
+    if (this.maxSize === this.items.length) {
+      return true;
+    }
+    return false;
+  }
+
+  isEmpty() {
+    if (this.items.length === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  push(item: T) {
+    if (this.isFull()) {
+      throw new Error('stack full');
+    }
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('stack empty');
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error('stack empty');
+    }
+    return this.items[this.items.length - 1];
+  }
+}
