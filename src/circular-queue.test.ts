19c54c602e8361913ded48cf6b38cd07d954fa8f
henry <48340699+imwh0im@users.noreply.github.com>
2024-01-06 07:08:34 +0000

    circular queue

---
+++ src/circular-queue.test.ts
@@ -0,0 +1,29 @@
+import { CircularQueue } from "./circular-queue"
+
+describe('queue Test', () => {
+  it('Case 1', () => {
+    const queue = new CircularQueue<number>(5);
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    expect(queue.dequeue()).toEqual(1);
+    
+    expect(queue.isEmpty()).toBe(false);
+  });
+
+  it('Case 2', () => {
+    const queue = new CircularQueue<string>(5);
+    queue.enqueue('a');
+    queue.enqueue('b');
+    queue.enqueue('c');
+    queue.enqueue('d');
+    queue.enqueue('e');
+    expect(() => queue.enqueue('f')).toThrow('full queue');
+
+    const item = queue.dequeue();
+    expect(item).toEqual('a');
+
+    queue.enqueue('f')
+    expect(queue.isFull()).toBe(true);
+  });
+});
---
+++ src/circular-queue.ts
@@ -0,0 +1,46 @@
+export class CircularQueue<T> {
+  constructor(
+    private maxSize: number,
+  ) {
+    this.maxSize++;
+  }
+
+    private items: T[] =[];
+
+    private front: number = 0;
+    private rear: number = 0;
+
+    isEmpty() {
+      return this.front === this.rear;
+    }
+
+    isFull() {
+      return (this.rear + 1) % this.maxSize === this.front;
+    }
+
+    enqueue(item: T) {
+      if(this.isFull()) {
+        throw new Error('full queue');
+      }
+
+      this.rear = (this.rear + 1) % this.maxSize;
+      this.items[this.rear] = item;
+    }
+
+    dequeue() {
+      if(this.isEmpty()) {
+        throw new Error('empty queue');
+      }
+      this.front = (this.front + 1) % this.maxSize;
+      const item = this.items[this.front];
+      delete this.items[this.front];
+      return item;
+    }
+
+    peek(){
+      if (this.isEmpty()) {
+        throw new Error('empty queue');
+      }
+      return this.items[this.front + 1];
+    }
+}
